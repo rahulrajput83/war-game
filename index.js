fd83b0f4601f7aa5001829eeff3342ac183b3e47
rahulrajput83 <rahulrajput21112@gmail.com>
2022-05-05 22:10:43 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,161 @@
+const card_type = ['♠', '♥', '♦', '♣'];
+const card_value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+let mixed = card_value.flatMap(d => card_type.map(v => d + v));
+let shuffledArray = [];
+let card1 = document.getElementById("player");
+let card2 = document.getElementById("computer");
+
+function start() {
+    if (shuffledArray.length == 0) {
+        const card_type = ['♠', '♥', '♦', '♣'];
+        const card_value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+        let mixed = card_value.flatMap(d => card_type.map(v => d + v));
+        shuffledArray = mixed.sort((a, b) => 0.5 - Math.random());
+        document.getElementById("player_no").style.display = "none";
+        document.getElementById("com_no").style.display = "none";
+        document.getElementById("player_no").innerText = shuffledArray;
+        document.getElementById("com_no").innerText = shuffledArray;
+    }
+    else if (shuffledArray.length < 52) {
+        shuffledArray = shuffledArray.sort((a, b) => 0.5 - Math.random());
+        document.getElementById("player_no").style.display = "none";
+        document.getElementById("com_no").style.display = "none";
+    }
+    else if (shuffledArray.length == 52) {
+        const card_type = ['♠', '♥', '♦', '♣'];
+        const card_value = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+        let mixed = card_value.flatMap(d => card_type.map(v => d + v));
+        shuffledArray = mixed.sort((a, b) => 0.5 - Math.random());
+        document.getElementById("player_no").style.display = "none";
+        document.getElementById("com_no").style.display = "none";
+        document.getElementById("player_no").innerText = shuffledArray;
+        document.getElementById("com_no").innerText = shuffledArray;
+    }
+    document.getElementById("button1").disabled = false;
+    count = 0;
+    var x = setInterval(function(){
+        card1.classList.toggle("flipCard");
+        card2.classList.toggle("flipCard");
+        if(count > 20) clearInterval(x);
+        count++;
+    }, 70);
+};
+
+function getNo() {
+    if (shuffledArray.length == 0) {
+        document.getElementById("player_no").style.display = "none";
+        document.getElementById("com_no").style.display = "none";
+        document.getElementById("button1").disabled = true;
+        document.getElementById("player").style.backgroundImage = "url('background.jpg')";
+        document.getElementById("computer").style.backgroundImage = "url('background.jpg')";
+    }
+    else {
+        document.getElementById("player_no").style.display = "block";
+        document.getElementById("player").style.backgroundImage = "none";
+        document.getElementById("computer").style.backgroundImage = "none";
+        document.getElementById("com_no").style.display = "block";
+        document.getElementById("player_no").innerText = shuffledArray.pop();
+        document.getElementById("com_no").innerText = shuffledArray.pop();
+        let char1 = document.getElementById("player_no").innerText;
+        let char2 = document.getElementById("com_no").innerText;
+        let player = document.getElementById("player_score");
+        let player_score = player.innerHTML;
+        let computer = document.getElementById("computer_score");
+        let computer_score = computer.innerHTML;
+        document.getElementById("winner").innerText = "";
+        card1.classList.toggle("flipCard");
+        card2.classList.toggle("flipCard");
+        let num1 = 0;
+        let num2 = 0;
+
+
+        if ( char1.includes("A")) {
+            num1 = 11;
+        }
+        else if ( char1.includes("J")) {
+            num1 = 12;
+        }
+        else if ( char1.includes("Q")) {
+            num1 = 13;
+        }
+        else if ( char1.includes("K")) {
+            num1 = 14;
+        }
+        else if ( char1.includes("2")) {
+            num1 = 2;
+        }
+        else if ( char1.includes("3")) {
+            num1 = 3;
+        }
+        else if ( char1.includes("4")) {
+            num1 = 4;
+        }
+        else if ( char1.includes("5")) {
+            num1 = 5;
+        }
+        else if ( char1.includes("6")) {
+            num1 = 6;
+        }
+        else if ( char1.includes("7")) {
+            num1 = 7;
+        }
+        else if ( char1.includes("8")) {
+            num1 = 8;
+        }
+        else if ( char1.includes("9")) {
+            num1 = 9;
+        }
+        else if ( char1.includes("10")) {
+            num1 = 10;
+        }
+        if ( char2.includes("A")) {
+            num2 = 11;
+        }
+        else if ( char2.includes("J")) {
+            num2 = 12;
+        }
+        else if ( char2.includes("Q")) {
+            num2 = 13;
+        }
+        else if ( char2.includes("K")) {
+            num2 = 14;
+        }
+        else if ( char2.includes("2")) {
+            num2 = 2;
+        }
+        else if ( char2.includes("3")) {
+            num2 = 3;
+        }
+        else if ( char2.includes("4")) {
+            num2 = 4;
+        }
+        else if ( char2.includes("5")) {
+            num2 = 5;
+        }
+        else if ( char2.includes("6")) {
+            num2 = 6;
+        }
+        else if ( char2.includes("7")) {
+            num2 = 7;
+        }
+        else if ( char2.includes("8")) {
+            num2 = 8;
+        }
+        else if ( char2.includes("9")) {
+            num2 = 9;
+        }
+        else if ( char2.includes("10")) {
+            num2 = 10;
+        }
+        if (num1 > num2) {
+            player_score = +player_score + 1;
+            player.innerHTML = player_score;
+        }
+        else if (num1 < num2) {
+            computer_score = +computer_score + 1;
+            computer.innerHTML = computer_score;
+        }
+        else if (num1 == num2) {
+        }
+    }
+};
